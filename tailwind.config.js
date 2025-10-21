cecf8528f989041d34db1ba470dc979a6f15b93b
mlee <mlee@chefman.com>
2025-10-21 11:08:02 -0400

    added: app theme color

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,40 +9,40 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        // RecipeiQ brand colors from icon
+        // RecipeiQ brand colors - matching Fresh & Modern theme (Option 1)
         cream: {
-          DEFAULT: '#ebe8d4',
-          light: '#f5f3e4',
-          dark: '#e0dcc8',
+          DEFAULT: '#faf8f3',  // Light subtle cream
+          light: '#faf8f3',
+          dark: '#f5f3e4',
         },
         brand: {
-          50: '#e6f2ee',
-          100: '#cce5dd',
-          200: '#99cbbb',
-          300: '#66b199',
-          400: '#349777',
-          500: '#02533a',  // Main green from icon
-          600: '#024332',
-          700: '#013229',
-          800: '#012221',
-          900: '#011118',
+          50: '#ecfdf5',
+          100: '#d1fae5',
+          200: '#a7f3d0',
+          300: '#6ee7b7',
+          400: '#34d399',
+          500: '#10b981',  // Main bright emerald from Option 1
+          600: '#059669',
+          700: '#047857',
+          800: '#065f46',
+          900: '#064e3b',
         },
-        // Override default green with brand green
+        // Override default green with Fresh & Modern green
         green: {
-          50: '#e6f2ee',
-          100: '#cce5dd',
-          200: '#99cbbb',
-          300: '#66b199',
-          400: '#349777',
-          500: '#02533a',  // Main green from icon
-          600: '#024332',
-          700: '#013229',
-          800: '#012221',
-          900: '#011118',
+          50: '#ecfdf5',
+          100: '#d1fae5',
+          200: '#a7f3d0',
+          300: '#6ee7b7',
+          400: '#34d399',
+          500: '#10b981',  // Main bright emerald from Option 1
+          600: '#059669',
+          700: '#047857',
+          800: '#065f46',
+          900: '#064e3b',
         },
       },
       backgroundColor: {
-        DEFAULT: '#ebe8d4',  // Cream as default background
+        DEFAULT: '#faf8f3',  // Light subtle cream as default background
       },
     },
   },
